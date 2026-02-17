5fae0e7c02ae158ef88de5b3302fa634cf854640
Larissa Bertaze <larissabertaze@gmail.com>
2026-02-17 16:00:59 -0300

    🧮 Calculadora

--- script.js
+++ script.js
@@ -53,32 +53,11 @@ window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") clearDisplay();
 });
 
-const visor = document.getElementById("visor");
-let acabouDeCalcular = false;
-
-function adicionarNumero(numero) {
-  if (acabouDeCalcular) {
-    visor.value = "";
-    acabouDeCalcular = false;
-  }
-  visor.value += numero;
-}
-
-function adicionarOperador(op) {
-  acabouDeCalcular = false;
-  visor.value += op;
-}
-
 function calcular() {
   try {
     visor.value = eval(visor.value);
-    acabouDeCalcular = true;
+    acabouDeCalcular = true; // ← ativa
   } catch {
     visor.value = "Erro";
   }
-}
-
-function limpar() {
-  visor.value = "";
-  acabouDeCalcular = false;
 }
